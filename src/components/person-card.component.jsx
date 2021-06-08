644abc130975e152004d7f02cd7bfea7dd2ca1f0
unknown <yhmdstudent@gmail.com>
2021-06-08 18:48:01 +0300

    updates

---
+++ src/components/person-card.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './person-card.styles.scss';
+
+const PersonCard = () => {
+    return(
+        <div className='card'>
+            TEST PersonCard
+        </div>
+    )
+}
+
+export default PersonCard;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => {
+    return(
+        <div className='test'>
+            <h1>TEST hompage</h1>
+        </div>
+    )
+}
+
+export default HomePage;
