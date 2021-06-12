5212cc8023ac7c5ea7653b88dc0fb57c7fbc7fba
yaronhai <72712518+yaronhai@users.noreply.github.com>
2021-06-12 03:55:31 +0300

    adding sign in form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import HomePage from './pages/homepage/homepage.component';
 import Rooms from './pages/rooms/rooms.component';
 import {Route, Switch} from 'react-router-dom';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/Rooms' component={Rooms} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
       
     </div>
